7ec48c9cba520b9f7821711a75cc919e4db32442
GhosOfRML <ghostofrml9848@gmail.com>
2026-04-17 21:13:15 +0300

    Started 09 and made it work for 3 tests,continuing Sunday.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+// Do not edit below this line
+let sum= 0 ;
+    if (a<b){
+        for(let i = a ; i <= b; i++){
+         sum += i;
+        }
+    return sum;
+    }else if(a>b){
+        for(let i = b ; i <= a; i++){
+         sum += i;
+        }
+    return sum;
+    }
 };
 
 // Do not edit below this line
