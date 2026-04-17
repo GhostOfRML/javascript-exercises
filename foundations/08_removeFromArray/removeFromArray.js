77b14a9059482aaf8a8ba98847e3401170457de0
GhosOfRML <ghostofrml9848@gmail.com>
2026-04-17 19:29:55 +0300

    Finished it after reaserching for about 2 days, found a helpful trick online about "!" the not

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-    
-};
+const removeFromArray = function( array, ...args ) {
+/*Make a function that goes trough all the elements of an array ,and takes out a word or multiple
+It doesnt need to be in the same array but it can still be   
+*/   
+  return array.filter(item => {
+    return !args.includes(item);
+  });
+}  
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
